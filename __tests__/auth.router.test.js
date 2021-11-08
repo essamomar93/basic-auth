6edf1dd33b18cdc77f30582ebe3ee7bc8e447ee6
essam omar <essam.93@hotmail.com>
2021-11-08 16:41:35 +0400

    1

--- __tests__/auth.router.test.js
+++ __tests__/auth.router.test.js
@@ -2,9 +2,8 @@
 
 const { server } = require('../src/server'); // destructing assignment 
 const supertest = require('supertest');
+const base64= require('base-64')
 const mockRequest = supertest(server);
-const { db } = require('../src/server');
-
 
 
 describe('auth test', () => {
@@ -13,17 +12,19 @@ describe('auth test', () => {
             username: "essam",
             password: "1234"
         });
-        expect(response.status).toBe(201);
+        expect(response.status).toEqual(201);
     });
+    // ====================================
     it('signin to login as a user (use basic auth)', async () => {
         const response = await mockRequest.post('/signin').auth("essam", "1234");
-        expect(response.status).toBe(200);
+        expect(response.status).toEqual(200);
     });
-
+    // ====================================
     it('signin to login as a user (use basic auth)', async () => {
         const response = await mockRequest.post('/signin').auth("essam", "1234");
-        expect(response.status).toBe(200);
+        expect(response.status).toEqual(200);
     });
+    // =====================================
     describe(' Middleware', () => {
         it('send the basic header', async () => {
             await mockServer.post('/signup').send({
@@ -37,14 +38,13 @@ describe('auth test', () => {
             expect(response.status).toEqual(200);
         });
     });
+    // =====================================
     it('doesn\'t auth if the password is wrong', async () => {
         let response = await mockServer.post('/signin').set({
             Authorization: `Basic ${base64.encode('essam:12345')}`
         });
         expect(response.status).toEqual(500);
     });
-
-
 
 
 });
